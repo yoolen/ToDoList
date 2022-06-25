57391b8ab3e011931dd252ecb121b5b7ee140e96
Terry Chern <yoolen@gmail.com>
2022-06-25 18:09:10 -0400

    finished

---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.todolist
+
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    fun addTodo(todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)  // must notify recycler view
+    }
+
+    fun deleteDoneTodos() {
+        todos.removeAll { todo ->
+            todo.checked
+        }
+        notifyDataSetChanged()  // notify that the entire list is updated
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val currTodo = todos[position]
+        holder.itemView.apply {
+            tvTodoTitle.text = currTodo.title
+            cbDone.isChecked = currTodo.checked
+            toggleStrikeThrough(tvTodoTitle, currTodo.checked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                currTodo.checked = !currTodo.checked
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
